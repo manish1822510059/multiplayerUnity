67bc60ce50ce0800b3bc58a20df13373cc0f7f00
Manish <imanish1998@gmail.com>
2023-06-04 14:45:02 +0530

    SkyBox, timerbaseGameOver Done

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
-      [SerializeField]
-      private GameObject gameOverScreen;
+    [SerializeField]  public float countdownDuration = 60f;
+  
+    private float currentTime;
+    
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TextMeshProUGUI timerText;
 
+    
+    float timer = 0f; 
+
+    
+     private void Start()
+    {
+        currentTime = countdownDuration;
+    }
+
+   
 
     // Update is called once per frame
     public  void GameOverCheck(int curreentHp){
@@ -15,4 +30,30 @@ public class GameOver : MonoBehaviour
             Movement.PlayerMoving = false;
         }
     }
+   
+ private void Update()
+    {
+        if (currentTime > 1f && Movement.PlayerMoving)
+        {
+            currentTime -= Time.deltaTime;
+            timerText.text = FormatTime(currentTime);
+        }
+        else if(Movement.PlayerMoving)
+        {
+            currentTime = 0f; 
+            gameOverScreen.SetActive(true);
+            Movement.PlayerMoving = false;
+            Debug.Log("complete time");       
+        }
+    }
+
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        return string.Format("{0}:{1:00}", minutes, seconds);
+    }
+
+
+
 }
