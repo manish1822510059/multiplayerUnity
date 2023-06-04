9b5a9f40c42998006768d86ead48240517912df0
Manish <imanish1998@gmail.com>
2023-06-04 10:27:13 +0530

    healthbar, collision

---
+++ Assets/Scripts/BoxCollider.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCollider : MonoBehaviour
+{
+
+public HpBarUi hpBarUi;
+    // Start is called before the first frame update
+ private void OnTriggerEnter(Collider other) {
+    if(other.gameObject.tag =="Enemy"){
+                hpBarUi.Attack(10);
+                print("hit enemy");
+    }
+    
+}
+
+}
---
+++ Assets/Scripts/HpBarUi.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarUi : MonoBehaviour
+{
+    public Image filledBar;
+    public Gradient gradient;
+
+    public Text hpText;
+
+    public int minHp,maxHp;
+    private int curreentHp;
+
+    private void Start() {
+        curreentHp = maxHp;
+        UpdateUI();
+    }
+
+    public void Heal(int point){
+       curreentHp = Mathf.Clamp(curreentHp+point,minHp,maxHp);
+       UpdateUI();
+    }
+
+    public void Attack(int point){ 
+       curreentHp = Mathf.Clamp(curreentHp-point,minHp,maxHp);
+       UpdateUI();
+
+    }
+    // Update is called once per frame
+    void UpdateUI()
+    {
+        hpText.text = curreentHp.ToString();
+        if(maxHp == 0){
+            Debug.Log("max Hp Can't be 0");
+            return;
+        }
+        filledBar.fillAmount = (float)curreentHp/maxHp;
+
+        filledBar.color = gradient.Evaluate(filledBar.fillAmount);
+    }
+}
